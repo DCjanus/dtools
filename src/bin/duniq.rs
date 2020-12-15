18c27c1404c890ec6ed905162e35b37797a2dc89
DCjanus <DCjanus@dcjanus.com>
2020-12-15 11:28:28 +0800

    uniq count

--- src/bin/duniq.rs
+++ src/bin/duniq.rs
@@ -6,10 +6,17 @@ use clap::Clap;
 use dtools::AnyResult;
 
 #[derive(Debug, Clap)]
-struct Command {}
+struct Command {
+    /// prefix lines by the number of occurrences
+    #[clap(short, long)]
+    count: bool,
+}
 
 fn main() -> AnyResult {
     let cmd: Command = Clap::parse();
+
+    let print_count = if cmd.count { 0 } else { 1 };
+
     let mut counter: AHashMap<_, u64> = Default::default();
     for line in std::io::stdin().lock().lines() {
         let line = line?;
@@ -17,8 +24,14 @@ fn main() -> AnyResult {
         let count = counter.entry(line.clone()).or_default();
         *count += 1;
 
-        if *count == 1 {
+        if *count == print_count {
             print!("{}\n", line.as_str());
+        }
+    }
+
+    if cmd.count {
+        for (k, v) in counter.iter() {
+            print!("{:>8} {}\n", v, k.as_str());
         }
     }
 
