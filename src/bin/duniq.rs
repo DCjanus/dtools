8fd4b2bcd79003e3880f1964a58609c1ad0e77db
DCjanus <DCjanus@dcjanus.com>
2023-01-24 12:49:09 +0800

    no more unexpected eof

--- src/bin/duniq.rs
+++ src/bin/duniq.rs
@@ -19,7 +19,11 @@ fn main() -> AnyResult {
 
     let mut counter: AHashMap<_, u64> = Default::default();
     for line in std::io::stdin().lock().lines() {
-        let line = line?;
+        let line = match line {
+            Ok(x) => x,
+            Err(e) if e.kind() == std::io::ErrorKind::UnexpectedEof => break,
+            Err(e) => Err(e)?,
+        };
         let line = std::rc::Rc::new(line);
         let count = counter.entry(line.clone()).or_default();
         *count += 1;
